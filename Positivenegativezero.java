20281cb62cd16c53caabfaceb6a87779a2de7158
Pauline Samyuktha R <paulinesamyuktha.26it@licet.ac.in>
2026-01-26 18:27:47 +0530

    7

---
+++ Positivenegativezero.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num==0){
+            System.out.println("zero");
+            
+        }
+        else if(num<0){
+            System.out.println("negative");
+        }
+        else{
+            System.out.println("postive");
+        }
+    }
+}
